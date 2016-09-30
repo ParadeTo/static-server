7237cb419936b5d9948ed424d97760fdf4954920
ayou <ayou@ayoudeMac-mini.local>
2016-09-30 10:55:12 +0800

    添加事件处理机制

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var events = require('events');
+var url = require('url');
+var proxy = new events.EventEmitter();
 var mime = require('./mime').types;
 var PORT = 8000;
 var staticPath = 'public'
@@ -10,16 +13,14 @@ var server = http.createServer(function(request, response) {
 	var realPath = staticPath + pathname;
 	 fs.exists(realPath, function (exists) {
 		if (!exists) {
-			response.writeHead(404, {'Content-Type': 'text/plain'});
-			response.write("This request URL " + pathname + " was not found on this server.");
-			response.end()
+			proxy.emit('not-found',response,pathname);
 		} else {
 			fs.readFile(realPath, "binary", function(err, file) {
 				if (err) {
-					response.writeHead(500, {'Content-Type': 'text/plain'});
-					response.end(err);
+					proxy.emit('inner-err',response,err);
 				} else {
-					response.writeHead(200, {'Content-Type': 'text/html'});
+					var contentType = getMimeType(pathname);
+					response.writeHead(200, {'Content-Type': contentType});
 					response.write(file, "binary");
 					response.end();
 				}
@@ -34,6 +35,20 @@ function getMimeType(filename) {
 	ext = ext ? ext.slice(1) : 'unknown';
 	return mime[ext] || "text/plain";
 }
+
+// 事件处理
+// 没有找到文件
+proxy.on('not-found',function(res, pathname) {
+	res.writeHead(404, {'Content-Type': 'text/plain'});
+	res.write("This request URL " + pathname + " was not found on this server.");
+	res.end()
+});
+// 服务器内部错误
+proxy.on('inner-err',function(res, err) {
+	res.writeHead(500, {'Content-Type': 'text/plain'});
+	res.end(err);
+});
+
 
 // 监听端口
 server.listen(PORT);
